6cc7ef16c331e3cfebd09b1f5dec8f076dc37a6a
MegaDen58 <extreme.den2004@gmail.com>
2023-03-22 22:24:31 +0300

    w

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+class Car 
+{
+    public string Name { get; set; }
+
+    public Car(string name)
+    {
+        Name = name;
+    }
+}
+
+class Garage 
+{
+    public List<Car> cars = new List<Car>();
+
+    public Garage(params Car[] autos)
+    {
+        foreach(Car car in autos)
+        {
+            cars.Add(car);
+        }
+    }
+}
+
+class Washer
+{
+    public void Wash(Car car)
+    {
+        Console.WriteLine($"Автомобиль {car.Name} помыт.");
+    }
+}
+
+class HelloWorld
+{
+    static void Main()
+    {
+        Garage garage = new Garage(new Car("Auto"), new Car("New Auto"));
+        Washer washer = new Washer();
+        
+        CarWasher carWasher = washer.Wash;
+
+        foreach(Car car in garage.cars)
+        {
+            carWasher(car);
+        }
+    }
+
+    delegate void CarWasher(Car car);
+}
